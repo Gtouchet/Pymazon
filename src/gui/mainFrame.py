3ee19b403c7a43c5abe211d987e9514c0c1d0e7a
David <darnaud2@myges.fr>
2021-07-08 02:07:03 +0200

    [Fix] add onglet with main frame

--- src/gui/mainFrame.py
+++ src/gui/mainFrame.py
@@ -1,14 +1,52 @@
 import tkinter as tk
-from src.gui.controllers.userView import create_widgets
+from tkinter import ttk
+from tkinter.ttk import Label
+
+from src.gui.controllers.home import home_interface
+from src.gui.controllers.graphic import graphic_interface
+from src.gui.controllers.db import db_interface
+from src.gui.controllers.services import services_interface
 
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.master = master
         self.pack()
-        create_widgets(self)
+        master.title("PYMAZON")
+        master.geometry("1200x600")
+        tab_control = ttk.Notebook(master)
+
+        #init onglet
+        home = ttk.Frame(tab_control)
+        graphic = ttk.Frame(tab_control)
+        DB = ttk.Frame(tab_control)
+        Services = ttk.Frame(tab_control)
+
+
+        #Create onglet
+        tab_control.add(home, text='Accueil')
+        tab_control.add(graphic, text='Graphic')
+        tab_control.add(DB, text='DB')
+        tab_control.add(Services, text='Services')
+
+        home_interface(home)
+        graphic_interface(graphic)
+        db_interface(DB)
+        services_interface(Services)
+
+        tab_control.pack(expand=1, fill='both')
+
+
 
 def launchApp():
     root = tk.Tk()
     app = Application(master=root)
     app.mainloop()
+
+
+
+
+
+
+
+
