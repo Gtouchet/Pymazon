c3f1f4c1f483568c92d4e06fc8ab94ac7126f925
Briand <briandthibaud99@gmail.com>
2021-07-10 16:35:30 +0200

    Implement Factory Product && category

---
+++ src/factory/GenerateDataProduct.py
@@ -0,0 +1,24 @@
+import random
+
+import faker
+from faker import Faker
+
+from src.controllers.cruds.productCrud.create import createProduct
+from src.models.product import Product
+from src.models.tag import Tag
+
+
+class GenerateDataProduct:
+    def __init__(self):
+        self.fake = Faker()
+
+    def generateData(self, nbProduct):
+        for i in range(nbProduct):
+            tag = Tag.get(random.randint(1, 23))
+            newProduct = Product()
+            newProduct.name = tag.name + '-' + tag.category.name + '-' + self.fake.md5()
+            newProduct.description = self.fake.text()
+            newProduct.price = random.randint(1, 1335)
+            newProduct.tag = tag.id
+            newProduct.category = tag.category.id
+            createProduct(newProduct)
