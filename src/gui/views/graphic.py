1606542b07932366bd7ba32a5d2457e5c38c0acb
David <darnaud2@myges.fr>
2021-07-19 00:22:43 +0200

    [Fix] bouton choise and data change

--- src/gui/views/graphic.py
+++ src/gui/views/graphic.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 from tkinter.ttk import Treeview
 
 import numpy as np
@@ -8,22 +9,14 @@ from matplotlib.figure import Figure
 
 
 def graphic_interface(self):
-    send1 = Button(self, text="Graph round", width=10, command=lambda: choise_Graph(1,self))
-    send1.place(x=30, y=20)
-    send2 = Button(self, text="Graph bar", width=10, command=lambda: choise_Graph(2,self))
-    send2.place(x=30, y=100)
-    send3 = Button(self, text="graph linear", width=10, command=lambda: choise_Graph(3,self))
-    send3.place(x=30, y=200)
+
+    drop_down_menu_graph(self)
+    drop_down_menu_Categorie(self)
+    drop_down_menu_Tag(self)
+    graph_plot3(self)
     grid_info(self)
 
 
-def choise_Graph(choise,self):
-    if choise == 1:
-        graph_plot(self)
-    elif choise == 2:
-        graph_plot2(self)
-    elif choise == 3:
-        graph_plot3(self)
 
 
 
@@ -35,7 +28,7 @@ def graph_plot(self):
     labels = 'france', 'belgique', 'amsterdam'
     sizes = [50, 25, 25]
     colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue', 'Orange', 'red']
-    explode = (0.2, 0, 0)  # explode 1st slice (Ireland), makes it more prominent
+    explode = (0, 0.2, 0)  # explode 1st slice (Ireland), makes it more prominent
 
     # Plot pie chart
     plt.pie(sizes, explode=explode, labels=labels, colors=colors, autopct='%1.1f%%', shadow=True, startangle=140)
@@ -54,7 +47,7 @@ def graph_plot2(self):
     fig = plt.figure(figsize=(5, 5), dpi=80)
     labels = ("france", "belgique", "amsterdam")
     labelpos = np.arange(len(labels))
-    countrysum = [0,504,420]
+    countrysum = [0, 504, 420]
 
     ##This section formats the barchart for output
 
@@ -84,8 +77,8 @@ def graph_plot2(self):
 def graph_plot3(self):
     fig = Figure(figsize=(5, 4), dpi=100)
     t = ["france", "belgique", "amsterdam"]
-    s = [0,504,420]
-    fig.add_subplot(111).plot(t,s)
+    s = [0, 504, 420]
+    fig.add_subplot(111).plot(t, s)
 
     canvas = FigureCanvasTkAgg(fig, master=self)  # A tk.DrawingArea.
     canvas.draw()
@@ -95,23 +88,99 @@ def graph_plot3(self):
         canvas.delete('ALL')
 
 
-
 def grid_info(self):
+    resultat = [(0, "france", "60M", "250M"), (1, "belgique", "50M", "250M"), (2, "amsterdam", "80M", "500M")]
 
-    resultat = [(0,"france","60M","250M"),(1,"belgique","50M","250M"),(2,"amsterdam","80M","500M")]
-
-
-    #header
-    tableau = Treeview(self, columns=('Pays', 'NP',"CA"))
+    # header
+    tableau = Treeview(self, columns=('Pays', 'NP', "CA"))
     tableau.heading('Pays', text='Pays')
     tableau.heading('NP', text="Nombre d'habitant")
     tableau.heading('CA', text="Chiffre d'affaire")
-    tableau['show'] = 'headings'  # sans ceci, il y avait une colonne vide à gauche qui a pour rôle d'afficher le paramètre "text" qui peut être spécifié lors du insert
+    tableau[
+        'show'] = 'headings'  # sans ceci, il y avait une colonne vide à gauche qui a pour rôle d'afficher le paramètre "text" qui peut être spécifié lors du insert
     tableau.pack(padx=3, pady=(0, 2))
 
-    #content
+    # content
     for enreg in resultat:
         # chaque ligne n'a pas de parent, est ajoutée à la fin de la liste, utilise le champ id comme identifiant et on fournit les valeurs pour chacune des colonnes du tableau
-        tableau.insert('', 'end', iid=enreg[0], values=(enreg[1], enreg[2],enreg[3]))
+        tableau.insert('', 'end', iid=enreg[0], values=(enreg[1], enreg[2], enreg[3]))
 
     tableau.place(relx=0.7, rely=0.80, anchor=CENTER)
+
+
+def drop_down_menu_graph(self):
+    OptionList = [
+        "Graph",
+        "graph_round",
+        "graph_bar",
+        "graph_linear"
+
+    ]
+
+
+    variable = tk.StringVar(self)
+    variable.set(OptionList[0])
+
+
+
+    opt = tk.OptionMenu(self, variable, *OptionList)
+    opt.config(width=10, font=('Helvetica', 12))
+    opt.place(relx=0.05, rely=0.05, anchor=CENTER)
+
+    labelTest = tk.Label(text="", font=('Helvetica', 12), fg='red')
+    labelTest.place(anchor=CENTER)
+
+    def callback(*args):
+        if format(variable.get()) == "graph_round":
+            graph_plot(self)
+        elif format(variable.get()) == "graph_bar":
+            graph_plot2(self)
+        elif format(variable.get()) == "graph_linear":
+            graph_plot3(self)
+
+
+
+
+
+    variable.trace("w", callback)
+
+
+def drop_down_menu_Categorie(self):
+    OptionList = [
+        "Category",
+        "test2",
+        "test3",
+        "test4"
+    ]
+
+
+    variable = tk.StringVar(self)
+    variable.set(OptionList[0])
+
+    opt = tk.OptionMenu(self, variable, *OptionList)
+    opt.config(width=10, font=('Helvetica', 12))
+    opt.place(relx=0.15, rely=0.05, anchor=CENTER)
+
+    labelTest = tk.Label(text="", font=('Helvetica', 12), fg='red')
+    labelTest.place(anchor=CENTER)
+
+
+def drop_down_menu_Tag(self):
+    OptionList = [
+        "TAG",
+        "test2",
+        "test3",
+        "test4"
+    ]
+
+
+    variable = tk.StringVar(self)
+    variable.set(OptionList[0])
+
+    opt = tk.OptionMenu(self, variable, *OptionList)
+    opt.config(width=10, font=('Helvetica', 12))
+    opt.place(relx=0.25, rely=0.05, anchor=CENTER)
+
+    labelTest = tk.Label(text="", font=('Helvetica', 12), fg='red')
+    labelTest.place(anchor=CENTER)
+
