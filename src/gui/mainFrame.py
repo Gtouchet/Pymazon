3911d60b50f4dfcbec8bc9214c3613f772f7a3fa
Touchet <gtouchet@myges.fr>
2021-07-19 19:40:19 +0200

    [Init] init branch
    added a tab

--- src/gui/mainFrame.py
+++ src/gui/mainFrame.py
@@ -20,12 +20,14 @@ class Application(tk.Frame):
         graphic = ttk.Frame(tab_control)
         DB = ttk.Frame(tab_control)
         mailSender = ttk.Frame(tab_control)
+        statistics = ttk.Frame(tab_control)
 
         #Create onglet
         tab_control.add(home, text='Accueil')
         tab_control.add(graphic, text='Graphic')
         tab_control.add(DB, text='DB')
         tab_control.add(mailSender, text='Mail Sender')
+        tab_control.add(statistics, text='Statistics')
 
         home_interface(home)
         graphic_interface(graphic)
