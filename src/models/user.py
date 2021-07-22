a98d6617f6edfaccd01c5d0cbe43617d4e9c5d9a
Briand <briandthibaud99@gmail.com>
2021-07-22 22:33:34 +0200

    Update UserModel

--- src/models/user.py
+++ src/models/user.py
@@ -1,3 +1,5 @@
+import datetime
+
 import peewee as pw
 
 db = pw.SqliteDatabase('pymazon.db', pragmas={'foreign_keys': 1})
@@ -6,8 +8,8 @@ class User(pw.Model):
     id = pw.AutoField()
     firstName = pw.CharField()
     lastName = pw.CharField()
-    address = pw.CharField()
+    address = pw.CharField(default="")
     mailAddress = pw.CharField()
-    createdDate = pw.DateTimeField()
+    createdDate = pw.DateTimeField(default=datetime.datetime.now())
     class Meta:
         database = db
