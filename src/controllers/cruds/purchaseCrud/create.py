c6f14238a7e0ee48457f0ddefefae0d83020b958
Briand <briandthibaud99@gmail.com>
2021-07-22 22:35:27 +0200

    Update PurchaseCrud

--- src/controllers/cruds/purchaseCrud/create.py
+++ src/controllers/cruds/purchaseCrud/create.py
@@ -1,3 +1,8 @@
+from src.services.logger import Logger
+
 
 def createPurchase(purchase):
+    Logger("CreatePurchase", {
+        "subject": "Create of " + purchase.name,
+    })
     purchase.save()
