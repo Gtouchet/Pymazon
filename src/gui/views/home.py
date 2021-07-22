e3e8a5c07aa5796665d384364d6a92172a4261a1
David <darnaud2@myges.fr>
2021-07-22 02:02:45 +0200

    [Fix] update home and resize frame

--- src/gui/views/home.py
+++ src/gui/views/home.py
@@ -8,29 +8,25 @@ def home_interface(self):
 
 
 def explain_onglet_graph(self):
-    textWelcome = "Bonjour est bienvenue sur Pymazon, Vous pouvez decouvrir notre interface de gestion"
-    tk.Label(self, text=textWelcome).place(x=200, y=20,width=50,height=150)
+    textWelcome = "👋 Bonjour est bienvenue sur Pymazon, Vous pouvez decouvrir notre interface de gestion regroupant :"
+    tk.Label(self, text=textWelcome).place(x=250, y=20,width=750,height=50)
+
+
+    textOngletGraph = "📊 Onglet Graphique  \n \n Les logs vous permettez de voir les differents \n actions réalisé sur l'application Pymazon \n notament les logs des envoye de mail vis a vis des clients etc.."
+    tk.Label(self, text=textOngletGraph).place(x=30, y=160,width=450,height=250)
+
+
+    textOngletDB =  " 📥 Onglet DB \n \n Les logs vous permettez de voir les differents \nactions réalisé sur l'application Pymazon \n notament les logs des envoye de mail vis a vis des clients etc.."
+    tk.Label(self,text=textOngletDB).place(x=700, y=160,width=450,height=250)
 
 
 
-    textPresentation = "Notre interface posséde de nombreuses fonctionnalités"
-    tk.Label(self, text=textPresentation).place(x=40, y=200)
+    textOngletMail = " 📨 Onglet Mail \n \n Les logs vous permettez de voir les differents \nactions réalisé sur l'application Pymazon  notament \n les logs des envoye de mail vis a vis des clients etc.."
 
-    textOngletGraph = "Onglet Graphique\n Les logs vous permettez de voir les differents actions réalisé sur l'application Pymazon \n notament les logs des envoye de mail vis a vis des clients etc.."
-    tk.Label(self, text=textOngletGraph).place(x=400, y=10,width=450,height=250)
-
-
-    textOngletDB =  "Onglet DB \n Les logs vous permettez de voir les differents actions réalisé sur l'application Pymazon \n notament les logs des envoye de mail vis a vis des clients etc.."
-    tk.Label(self,text=textOngletDB).place(x=30, y=200,width=450,height=250)
+    tk.Label(self, text=textOngletMail).place(x=30, y=450,width=450,height=250)
 
 
 
-    textOngletMail = "Onglet Mail\n Les logs vous permettez de voir les differents \nactions réalisé sur l'application Pymazon  notament \n les logs des envoye de mail vis a vis des clients etc.."
+    textOngletLog = " 🧾 Onglet LOG \n \n Les logs vous permettez de voir les differents \n actions réalisé sur l'application Pymazon notament \n les logs des envoye de mail vis a vis des clients etc.."
 
-    tk.Label(self, text=textOngletMail).place(x=200, y=200,width=450,height=250)
-
-
-
-    textOngletLog = "Onglet LOG \n \n Les logs vous permettez de voir les differents \n actions réalisé sur l'application Pymazon notament \n les logs des envoye de mail vis a vis des clients etc.."
-
-    tk.Label(self, text=textOngletLog).place(x=650, y=400, width=450,height=250)
+    tk.Label(self, text=textOngletLog).place(x=700, y=450, width=450,height=250)
